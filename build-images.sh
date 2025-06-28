04cc5ba20ac86a06c9d8647a5074395003ed0699
Kemboi Elvis <kemboielvis22@gmail.com>
2025-06-28 09:54:25 +0300

    refactor: replaced all kickstart names with couchdb

--- build-images.sh
+++ build-images.sh
@@ -18,17 +18,17 @@ reponame="couchdb"
 # Create a new empty container image
 container=$(buildah from scratch)
 
-# Reuse existing nodebuilder-kickstart container, to speed up builds
-if ! buildah containers --format "{{.ContainerName}}" | grep -q nodebuilder-kickstart; then
+# Reuse existing nodebuilder-couchdb container, to speed up builds
+if ! buildah containers --format "{{.ContainerName}}" | grep -q nodebuilder-couchdb; then
     echo "Pulling NodeJS runtime..."
-    buildah from --name nodebuilder-kickstart -v "${PWD}:/usr/src:Z" docker.io/library/node:lts
+    buildah from --name nodebuilder-couchdb -v "${PWD}:/usr/src:Z" docker.io/library/node:lts
 fi
 
 echo "Build static UI files with node..."
 buildah run \
     --workingdir=/usr/src/ui \
     --env="NODE_OPTIONS=--openssl-legacy-provider" \
-    nodebuilder-kickstart \
+    nodebuilder-couchdb \
     sh -c "yarn install && yarn build"
 
 # Add imageroot directory to the container image
--- ui/README.md
+++ ui/README.md
@@ -1,3 +1,3 @@
-# NS8 kickstart UI development
+# NS8 couchdb UI development
 
-To develop kickstart UI please refer to [this section of the Developer manual](https://nethserver.github.io/ns8-core/ui/modules/#module-ui-development).
+To develop couchdb UI please refer to [this section of the Developer manual](https://nethserver.github.io/ns8-core/ui/modules/#module-ui-development).
